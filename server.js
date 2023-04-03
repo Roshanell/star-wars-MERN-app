24716e9036b34fe7323b439f825d7e2bd3228d37
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-04-02 22:32:04 -0500

    entries updating in db

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 // require express
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 require("dotenv").config();
-// Body-parser is a middleware. They help to tidy up the request object before we use them. Express lets us use middleware with the use method.
-// The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const MongoClient = require("mongodb").MongoClient;
-let dbConnectionString = process.env.DB_String,
-	dbName = "star-wars",
-	collection;
-// allows us to connect to Mongo's client
-MongoClient.connect(dbConnectionString)
+const dbConnectionString = process.env.DB_String;
+
+MongoClient.connect(dbConnectionString, { useUnifiedTopology: true })
 	//allows us to connect to mongodb. Pass the connection string so it knows what DB to use
 	.then((client) => {
-		console.log("Connected to Database");
-		db = client.db(dbName);
-		collection = db.collection("quotes");
-	});
+		const dbName = "star-wars";
+		const db = client.db(dbName);
+		const quotesCollection = db.collection("quotes");
+		console.log(`Connected to Database ${dbName}`);
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
-	console.log(__dirname);
-	// Note: __dirname is the current directory you're in. Try logging it and see what you get!
-});
+		// middleware
+		// Body-parser is a middleware. They help to tidy up the request object before we use them. Express lets us use middleware with the use method.
+		// The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
+		app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/quotes", (req, res) => {
-	console.log(req.body);
-});
-/// creates a server that the browser can connect to
-app.listen(3000, function () {
-	console.log("listening on 3000");
-});
+		app.get("/", (req, res) => {
+			res.sendFile(__dirname + "/index.html");
+			//console.log(__dirname);
+			// Note: __dirname is the current directory you're in. Try logging it and see what you get!
+		});
+
+		app.post("/quotes", (req, res) => {
+			quotesCollection
+				.insertOne(req.body)
+				.then((result) => {
+					console.log(result);
+					res.redirect("/");
+				})
+				.catch((error) => console.error(error));
+		});
+		/// creates a server that the browser can connect to
+		app.listen(3000, function () {
+			console.log("listening on 3000");
+		});
+	})
+	.catch((error) => console.error(error));
